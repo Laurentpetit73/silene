730dc70d2943efa7dfc27427c8d5be775c6c5c2a
laurentpetit73 <petit.laurent73@gmail.com>
2020-10-22 09:49:24 +0200

    ajout formulaire login admin

---
+++ src/Controller/AdminAccountController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminAccountController extends AbstractController
+{
+    /**
+     * @Route("/admin", name="admin_login")
+     */
+    public function index()
+    {
+        return $this->render('admin/account/login.html.twig', [
+            'controller_name' => 'bg lolo',
+        ]);
+    }
+}
